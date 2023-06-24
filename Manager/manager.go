b395405f2fecb2fbc9b93292dad74e9fdf8ef120
kamran miralami <kamran.m1380@yahoo.com>
2023-06-24 22:42:54 +0330

    Update manager.go

--- Manager/manager.go
+++ Manager/manager.go
@@ -27,25 +27,25 @@ func GetNewCouponId() int64 {
 	err := CounterCollection.FindOne(context.Background(), filter).Decode(&result)
 	if err == mongo.ErrNoDocuments {
 		var tmp Cnt
-		tmp.cnt = 0
+		tmp.Cnt = 0
 		tmp.ID = "counter"
 		_, err := CounterCollection.InsertOne(context.Background(), tmp)
 		if err != nil {
 			log.Fatal(err)
 		}
-		return tmp.cnt
+		return tmp.Cnt
 	}
 	filter2 := bson.M{"_id": result.ID}
-	update2 := bson.M{"$set": bson.M{"cnt": result.cnt + 1}}
+	update2 := bson.M{"$set": bson.M{"cnt": result.Cnt + 1}}
 	res2, err := CounterCollection.UpdateOne(context.Background(), filter2, update2)
 	if err != nil {
 		log.Fatal(res2)
 		log.Fatal(err)
 	}
-	return result.cnt + 1
+	return result.Cnt + 1
 }
 
 type Cnt struct {
 	ID  string `json:"_id,omitempty" bson:"_id,omitempty"`
-	cnt int64  `json:"cnt,omitempty" bson:"cnt,omitempty"`
+	Cnt int64  `json:"cnt,omitempty" bson:"cnt,omitempty"`
 }
